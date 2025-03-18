5e6289599d4c01b3a2d2bf9ae5674b493f229f06
Helena Flausino <helenaflausino@gmail.com>
2025-03-17 23:41:57 -0300

    feat: adiciona opção de soma no menu

--- menu.py
+++ menu.py
@@ -13,11 +13,9 @@ def menu():
         if opcao == "0":
             print("Saindo...")
             break
+        
         elif opcao == "1":
             a = float(input("Digite o primeiro número: "))
             b = float(input("Digite o segundo número: "))
             print(f"Resultado: {calculadora.somar(a, b)}")
-        elif opcao == "2":
-            a = float(input("Digite o primeiro número: "))
-            b = float(input("Digite o segundo número: "))
-            print(f"Resultado: {calculadora.subtrair(a, b)}")
+
